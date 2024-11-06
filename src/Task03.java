d2d963108c9fee6bc3f93b98d2e8c4b8e06001d5
Alexandr Stepanenko <79439521+hellboy791@users.noreply.github.com>
2024-11-06 10:52:53 +0300

    Update Task03.java

--- src/Task03.java
+++ src/Task03.java
@@ -34,7 +34,7 @@ public class Task03 {
         int tempValue;
 
         for (int i = 0; i < array.length; i++){
-            for (int j = i + i; j < array.length; j++){
+            for (int j = i + 1; j < array.length; j++){
                 if (array[i] > array[j]){
                     tempValue = array[i];
                     array[i] = array[j];
